15bc018e549b3d222909e398c08e18629669ff30
safwakakeh <safwaak@uia.no>
2025-11-04 12:44:24 +0100

    MVC setting

---
+++ WebApplication1/Ikt201-Sultan_side/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ikt201_Sultan_side.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
---
+++ WebApplication1/Ikt201-Sultan_side/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Ikt201_Sultan_side.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
