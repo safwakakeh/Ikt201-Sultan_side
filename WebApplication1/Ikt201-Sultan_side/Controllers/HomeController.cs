f3818091fa98eafdacfd7a16f27fc41d08c064b7
Turki Alturki <turkiaa@uia.no>
2025-11-12 12:40:47 +0100

    navigation bar fixing

--- WebApplication1/Ikt201-Sultan_side/Controllers/HomeController.cs
+++ WebApplication1/Ikt201-Sultan_side/Controllers/HomeController.cs
@@ -18,7 +18,17 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
+    public IActionResult BordBestilling()
+    {
+        return View();
+    }
+
+    public IActionResult Meny()
+    {
+        return View();
+    }
+
+    public IActionResult KontaktOss()
     {
         return View();
     }
--- WebApplication1/Ikt201-Sultan_side/obj/Debug/net9.0/Ikt201-Sultan_side.AssemblyInfo.cs
+++ WebApplication1/Ikt201-Sultan_side/obj/Debug/net9.0/Ikt201-Sultan_side.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ikt201-Sultan_side")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15bc018e549b3d222909e398c08e18629669ff30")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ikt201-Sultan_side")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ikt201-Sultan_side")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
