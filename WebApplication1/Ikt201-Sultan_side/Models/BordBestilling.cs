6d6f4925d8a68764a807eacdd4ad088f31dc18ec
Camilla Smith-Tønnessen <camillahsm@uia.no>
2025-12-02 12:37:58 +0100

    halveis ferdig med frontend på home page og navbar

---
+++ WebApplication1/Ikt201-Sultan_side/Models/BordBestilling.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Ikt201_Sultan_side.Models;
+
+public class Bordbestilling
+{
+    public string Name { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public int Guests { get; set; }
+    public DateTime Time { get; set; }
+
+    public List<SelectListItem> GuestOptions { get; set; } =
+        Enumerable.Range(1, 10)
+            .Select(i => new SelectListItem { Value = i.ToString(), Text = i.ToString() })
+            .ToList();
+}
--- WebApplication1/Ikt201-Sultan_side/obj/Debug/net9.0/Ikt201-Sultan_side.AssemblyInfo.cs
+++ WebApplication1/Ikt201-Sultan_side/obj/Debug/net9.0/Ikt201-Sultan_side.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ikt201-Sultan_side")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08038a4efe7276aef76c8a220cb6563d08b0b5af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e605154cbd2ddd67ec58f9edfe62ea630a2cb857")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ikt201-Sultan_side")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ikt201-Sultan_side")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
