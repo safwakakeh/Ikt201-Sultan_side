3c722c8c20aa296f41ee1e1dfde37fa78d11fb93
Bardha Shalci <bardhas@uia.no>
2025-12-09 16:11:24 +0100

    Fikset databasekonflikter og migrasjoner

---
+++ WebApplication1/Ikt201-Sultan_side/Data/Migrations/20251209144917_FixSnapshot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ikt201_Sultan_side.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixSnapshot : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder) { }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder) { }
+    }
+}
